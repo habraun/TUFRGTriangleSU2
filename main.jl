02e6d05f24fdc6d6688c26d2364453db6cd2d241
habraun <64907913+habraun@users.noreply.github.com>
2022-08-01 14:06:06 +0200

    Add files via upload
    new main file so output files are updated

--- main.jl
+++ main.jl
@@ -1,6 +1,6 @@
 using Distributed
 rmprocs(workers())
-addprocs(4)
+addprocs(8)
 nworkers()
 
 
@@ -35,19 +35,23 @@ function init_flow!(
         t       ::Float64,
         t2      ::Float64,
         t3      ::Float64,
-        mu       ::Float64,
+        mu      ::Float64,
         Gamma   ::Bool,
         M       ::Bool,
-        K      ::Bool,
-        shell   ::Int64
+        K       ::Bool,
+        shell   ::Int64,
+        Vh      ::Float64,
+        Kh      ::Float64,
         )
 
         L               = 1+3*shell + 3*shell^2
         grid_bosons     = kgrid_initialization(N,shell,200,Gamma,M,K) # gamma,k,m
         bubbles         = bubbles_initialization(L,grid_bosons.N,shell)
         grid_r          = rgrid_initialization(Int64(ceil(sqrt(grid_bosons.N))))
-        fv              = fouriervertex_initialization(L,grid_r,shell)
-        v               = vertex_initialization(L,grid_bosons.N)
+        fv              = fouriervertex_initialization(L,grid_r,shell) #fw
+        v               = vertex_initialization(L,grid_bosons.N) #w #call them twice , once for v and once for w
+        fw              = fouriervertex_initialization(L,grid_r,shell)
+        w               = vertex_initialization(L,grid_bosons.N)
 
 
         println("Momenta:")
@@ -57,7 +61,7 @@ function init_flow!(
 
 
         #Calculate Flow
-        LambdaArr,pmax,cmax,dmax,BubblesGamma,BubblesM = start_flow(t,t2,t3,mu,U,V1,V2,V3,J,grid_bosons,bubbles,grid_r,v,fv)
+        LambdaArr,pmaxv,pmaxw,cmaxv,cmaxw,dmaxv,dmaxw,BubblesGamma,BubblesM = start_flow(t,t2,t3,mu,U,V1,V2,V3,J,grid_bosons,bubbles,grid_r,v,fv,w,fw,Vh,Kh)# add two arguments w and fw
 
         ################################################################################
         #prepare supplemental information for plotting
@@ -91,22 +95,37 @@ function init_flow!(
 
         #Use superconducting vertex c_sc to calculate leading gap functions
 
-        leadingvals,leadingvecs,sc,vPbuff=gapper(v,grid_bosons)
+        leadingvalsv,leadingvecsv,scv,vPbuffv=gapper(v,grid_bosons) #double it, once on v once on w
+        leadingvalsw,leadingvecsw,scw,vPbuffw=gapper(w,grid_bosons)
 
-        jldopen("triangle"*string(N)*string(U)*string(V1)*string(V2)*string(V3)*string(J)*string(round(t,digits=3))*string(round(t2,digits=3))*string(round(t3,digits=3))*string(round(mu,digits=3))*string(Gamma)*string(M)*string(K)*string(shell)*".jld", "w") do file
-            write(file, "SC", sc)  # alternatively, say "@write file A"
-            write(file, "VP", v.P)
-            write(file, "VPbuff", vPbuff)
+        jldopen("data/triangle"*string(N)*string(U)*string(V1)*string(V2)*string(V3)*string(J)*string(round(t,digits=3))*string(round(t2,digits=3))*string(round(t3,digits=3))*string(round(mu,digits=3))*string(Gamma)*string(M)*string(K)*string(shell)*".jld", "w") do file
+            write(file, "SCv", scv)  # alternatively, say "@write file A"
+            write(file, "VPv", v.P)
+            write(file, "VPbuffv", vPbuffv)
+            write(file, "SCw", scw)  # alternatively, say "@write file A"
+            write(file, "VPw", w.P)
+            write(file, "VPbuffw", vPbuffw)
         end
 
-        h5open("triangle"*string(N)*string(U)*string(V1)*string(V2)*string(V3)*string(J)*string(round(t,digits=3))*string(round(t2,digits=3))*string(round(t3,digits=3))*string(round(mu,digits=3))*string(Gamma)*string(M)*string(K)*string(shell)*".h5", "w") do file
-                write(file, "p", real.(v.P))
-                write(file, "c", real.(v.C))
-                write(file, "d", real.(v.D))
+        h5open("data/triangle_N_"*string(N)*"_U_"*string(U)*"_V1_"*string(V1)*"_V2_"*string(V2)*"_V3_"*string(V3)*"_J_"*string(J)*"_t_"*string(round(t,digits=3))*"_t2_"*string(round(t2,digits=3))*"_t3_"*string(round(t3,digits=3))*"_mu_"*string(round(mu,digits=3))*"_Gam_"*string(Gamma)*"_M_"*string(M)*"_K_"*string(K)*"_Sh_"*string(shell)*".h5", "w") do file
+                write(file, "pv", real.(v.P)) # save also w etc.
+                write(file, "cv", real.(v.C))
+                write(file, "dv", real.(v.D))
+                write(file, "pw", real.(w.P)) # save also w etc.
+                write(file, "cw", real.(w.C))
+                write(file, "dw", real.(w.D))
 
-                write(file, "pmax", abs.(pmax))
-                write(file, "cmax", abs.(cmax))
-                write(file, "dmax", abs.(dmax))
+                write(file, "pmaxv", abs.(pmaxv))
+                write(file, "cmaxv", abs.(cmaxv))
+                write(file, "dmaxv", abs.(dmaxv))
+                write(file, "pmaxw", abs.(pmaxw))
+                write(file, "cmaxw", abs.(cmaxw))
+                write(file, "dmaxw", abs.(dmaxw))
+
+                write(file, "leadingvecs1111v", leadingvecsv)
+                write(file, "leadingvals1111v", leadingvalsv)
+                write(file, "leadingvecs1111w", leadingvecsw)
+                write(file, "leadingvals1111w", leadingvalsw)
 
                 write(file, "Lambda", abs.(LambdaArr))
 
@@ -115,14 +134,18 @@ function init_flow!(
 
                 write(file,"BubblesGamma",BubblesGamma)
                 write(file,"BubblesM",BubblesM)
+                create_group(file,"Parameters")
+                write(file["Parameters"],"N",N)
+                write(file["Parameters"],"U",U)
+                write(file["Parameters"],"V1",V1)
+                write(file["Parameters"],"V2",V2)
+                write(file["Parameters"],"V3",V3)
+                write(file["Parameters"],"J",J)
+                write(file["Parameters"],"t",t)
+                write(file["Parameters"],"t2",t2)
+                write(file["Parameters"],"t3",t3)
+                write(file["Parameters"],"mu",mu)
+                write(file["Parameters"],"Shell",shell)
         end
-
-
-        h5open("triangleSC"*string(N)*string(U)*string(V1)*string(V2)*string(V3)*string(J)*string(round(t,digits=3))*string(round(t2,digits=3))*string(round(t3,digits=3))*string(round(mu,digits=3))*string(Gamma)*string(M)*string(K)*string(shell)*".h5", "w") do file
-                write(file, "leadingvecs1111", leadingvecs)
-                write(file, "leadingvals1111", leadingvals)
-                write(file,"coords",coordsarr)
-        end
-
 
 end
